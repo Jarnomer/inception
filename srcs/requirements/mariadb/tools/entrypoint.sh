b3bc759d5f985108cec516c07fb9be9973adf596
Your Name <you@example.com>
2025-01-13 14:14:09 +0200

    Reformats, worked on the wordpress, still does not exit loop but entrypoint.sh works now

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# Start the MariaDB service in the background
-service mariadb start
+# Start the service for setup
+mysqld_safe --user=root &
 
 # Wait for the MariaDB service to start
-until mariadb -e "SELECT 1;" > /dev/null 2>&1; do
+until mysqladmin ping --silent; do
     echo "Waiting for MariaDB to start..."
     sleep 2
 done
 
 # Check if the MariaDB database already exists
-if ! mariadb -e "USE $MARIA_DB_NAME;" 2>/dev/null; then
-    echo "Initializing MariaDB database and user..."
-    mariadb -u root < setup.sql
+if [ ! -d "/var/lib/mysql/$DB_NAME" ]; then
+    echo "Initializing MariaDB..."
+    mariadbd --initialize
+    mariadb -u root < /setup.sql
 else
     echo "MariaDB is already initialized."
 fi
 
-# Stop the MariaDB service
-service mariadb stop
+# Stop the service
+mysqladmin shutdown
--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-cd /var/www/html
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+while ! mysqladmin -h "$DB_NAME" -u"$DB_USER" -p"$DB_PASS" ping --silent; do
+    echo "Waiting for MariaDB to initialize..."
+    sleep 5
+done
+
+if [ ! -f /var/www/html/wp-config.php ]; then
+    echo "Initializing WordPress..."
+
+    # Download Wordpress
+    wp core download --allow-root
+
+    # Create configuration
+    wp core config --dbname=${WP_NAME} --dbuser=${WP_USER} \
+      --dbpass=${WP_PASS} --dbhost=${WP_HOST} --allow-root
+
+    # Install wordpress
+    wp core install --url=${DOMAIN_NAME} --title=${WP_TITLE} \
+      --admin_user=admin --admin_password=admin \
+      --admin_email=admin@admin.com --allow-root
+else
+    echo "WordPress is already initialized."
+fi
